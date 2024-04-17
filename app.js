47027308e0863ee0ccfb7e969aeac62b9f810890
bishn <bishnu.khulal@student.griffith.ie>
2024-04-17 01:47:23 +0100

    edited

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Import necessary packages
 const express = require('express');
-const expressLayout = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -32,30 +32,32 @@ app.use(methodOverride('_method')); // Enable HTTP method overriding
 
 // Configure session management
 app.use(session({
-  secret: 'keyboard cat', // Secret key for session encryption
+  secret: process.env.SESSION_SECRET || 'keyboard cat', // Use environment variable for session secret
   resave: false,
   saveUninitialized: true,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI // MongoDB connection URI
+    mongoUrl: process.env.MONGODB_URI, // MongoDB connection URI
+    dbName: process.env.MONGODB_DB_NAME, // Optional: Specify MongoDB database name
+    ttl: 7 * 24 * 60 * 60, // Optional: Session TTL (default is 14 days)
   }),
 }));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Configure Express to use EJS templating engine
-app.use(expressLayout);
+// Configure Express to use EJS templating engine with expressLayouts
+app.use(expressLayouts);
 app.set('layout', './layouts/main'); // Set default layout file
 app.set('view engine', 'ejs'); // Set view engine to EJS
 
 // Make isActiveRoute function available to all views
-app.locals.isActiveRoute = isActiveRoute; 
+app.locals.isActiveRoute = isActiveRoute;
 
 // Define routes
 app.use('/', require('./server/routes/main')); // Main routes
 app.use('/', require('./server/routes/admin')); // Admin routes
 
 // Start the server and listen on the defined port
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
