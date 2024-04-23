64b9b2105b442e9ef076c1d57d5f1f6e0a2f37f2
bishn <bishnu.khulal@student.griffith.ie>
2024-04-23 13:14:44 +0100

    edited

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,36 +1,42 @@
 const express = require('express');
-const router = express.Router(); // Creating an instance of Express router
-const Post = require('../models/Post'); // Importing the Post model
-const User = require('../models/User'); // Importing the User model
-const bcrypt = require('bcrypt'); // Importing bcrypt for password hashing
-const jwt = require('jsonwebtoken'); // Importing jwt for generating tokens
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
 
-const adminLayout = '../views/layouts/admin'; // Setting the layout file path for admin views
-const jwtSecret = process.env.JWT_SECRET; // Getting JWT secret from environment variables
+const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
-/**
- * Middleware function to check if user is authenticated
- */
-const authMiddleware = (req, res, next ) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const authMiddleware = (req, res, next) => {
   const token = req.cookies.token;
 
-  if(!token) {
-    return res.status(401).json( { message: 'Unauthorized'} );
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
   }
 
   try {
     const decoded = jwt.verify(token, jwtSecret);
     req.userId = decoded.userId;
     next();
-  } catch(error) {
-    res.status(401).json( { message: 'Unauthorized'} );
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
   }
-}
+};
 
-/**
- * GET /admin
- * Render login page for admin
- */
 router.get('/admin', async (req, res) => {
   try {
     const locals = {
@@ -44,27 +50,23 @@ router.get('/admin', async (req, res) => {
   }
 });
 
-/**
- * POST /admin
- * Authenticate admin user
- */
 router.post('/admin', async (req, res) => {
   try {
     const { username, password } = req.body;
-    
-    const user = await User.findOne( { username } );
 
-    if(!user) {
-      return res.status(401).json( { message: 'Invalid credentials' } );
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordValid) {
-      return res.status(401).json( { message: 'Invalid credentials' } );
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ userId: user._id}, jwtSecret );
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
     res.cookie('token', token, { httpOnly: true });
     res.redirect('/dashboard');
 
@@ -73,10 +75,6 @@ router.post('/admin', async (req, res) => {
   }
 });
 
-/**
- * GET /dashboard
- * Render admin dashboard
- */
 router.get('/dashboard', authMiddleware, async (req, res) => {
   try {
     const locals = {
@@ -97,10 +95,6 @@ router.get('/dashboard', authMiddleware, async (req, res) => {
 
 });
 
-/**
- * GET /add-post
- * Render form to add new post
- */
 router.get('/add-post', authMiddleware, async (req, res) => {
   try {
     const locals = {
@@ -120,36 +114,24 @@ router.get('/add-post', authMiddleware, async (req, res) => {
 
 });
 
-/**
- * POST /add-post
- * Add new post
- */
-router.post('/add-post', authMiddleware, async (req, res) => {
+router.post('/add-post', authMiddleware, upload.single('image'), async (req, res) => {
   try {
-    try {
-      const newPost = new Post({
-        title: req.body.title,
-        body: req.body.body
-      });
+    const newPost = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      image: req.file.filename
+    });
 
-      await Post.create(newPost);
-      res.redirect('/dashboard');
-    } catch (error) {
-      console.log(error);
-    }
+    await Post.create(newPost);
+    res.redirect('/dashboard');
 
   } catch (error) {
     console.log(error);
   }
 });
 
-/**
- * GET /edit-post/:id
- * Render form to edit post
- */
 router.get('/edit-post/:id', authMiddleware, async (req, res) => {
   try {
-
     const locals = {
       title: "Edit Post",
       description: "Free NodeJs User Management System",
@@ -169,13 +151,8 @@ router.get('/edit-post/:id', authMiddleware, async (req, res) => {
 
 });
 
-/**
- * PUT /edit-post/:id
- * Update existing post
- */
 router.put('/edit-post/:id', authMiddleware, async (req, res) => {
   try {
-
     await Post.findByIdAndUpdate(req.params.id, {
       title: req.body.title,
       body: req.body.body,
@@ -190,23 +167,19 @@ router.put('/edit-post/:id', authMiddleware, async (req, res) => {
 
 });
 
-/**
- * POST /register
- * Register new admin user
- */
 router.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-      const user = await User.create({ username, password:hashedPassword });
+      const user = await User.create({ username, password: hashedPassword });
       res.status(201).json({ message: 'User Created', user });
     } catch (error) {
-      if(error.code === 11000) {
-        res.status(409).json({ message: 'User already in use'});
+      if (error.code === 11000) {
+        res.status(409).json({ message: 'User already in use' });
       }
-      res.status(500).json({ message: 'Internal server error'})
+      res.status(500).json({ message: 'Internal server error' })
     }
 
   } catch (error) {
@@ -214,29 +187,18 @@ router.post('/register', async (req, res) => {
   }
 });
 
-/**
- * DELETE /delete-post/:id
- * Delete post
- */
 router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
-
   try {
-    await Post.deleteOne( { _id: req.params.id } );
+    await Post.deleteOne({ _id: req.params.id });
     res.redirect('/dashboard');
   } catch (error) {
     console.log(error);
   }
-
 });
 
-/**
- * GET /logout
- * Logout admin user
- */
 router.get('/logout', (req, res) => {
   res.clearCookie('token');
   res.redirect('/');
 });
 
-// Exporting the router module
 module.exports = router;
